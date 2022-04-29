53605db4dc71712758e8e04205eae5a49b85e17d
Daria Kamenskaya <daria.kamenskaya@yandex.ru>
2022-04-29 18:06:38 +0300

    feat: выровнять верстку по pixelperfect для главной страницы для разрешения 1280px

--- src/components/Techs/Techs.js
+++ src/components/Techs/Techs.js
@@ -2,7 +2,7 @@ function Techs() {
 
     return (
       <section className="techs">
-          <h2 className={'section__header'}>Технологии</h2>
+          <h2 className={'section__header section__header_tech'}>Технологии</h2>
           <h3 className={'techs__header'}>7 технологий</h3>
           <p className={'techs__text'}>На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
           <section className={'techs__table'}>
