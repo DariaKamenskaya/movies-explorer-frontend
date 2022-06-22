f030dfccfeae524eb53b936c43753e59356357ed
Daria Kamenskaya <daria.kamenskaya@yandex.ru>
2022-06-22 19:07:44 +0300

    feat: добавить ссылки в блоке портфолио

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -8,15 +8,15 @@ function Portfolio() {
       <div className={'portfolio__nav'}>
         <li className={'portfolio__nav-element'}>
           <h4 className={'portfolio__link'}>Статичный сайт</h4>
-          <Link to={'/sign-up'} className={'portfolio__link'}>{'↗'}</Link>
+          <a href="https://dariakamenskaya.github.io/how-to-learn" rel="noopener noreferrer" target="_blank" className="portfolio__link">{'↗'}</a>
         </li>
         <li className={'portfolio__nav-element'}>
           <h4 className={'portfolio__link'}>Адаптивный сайт</h4>
-          <Link to={'/sign-up'} className={'portfolio__link'}>{'↗'}</Link>
+          <a href="https://dariakamenskaya.github.io/russian-travel_kdd/index.html" rel="noopener noreferrer" target="_blank" className="portfolio__link">{'↗'}</a>
         </li>
         <li className={'portfolio__nav-element'}>
           <h4 className={'portfolio__link'}>Одностраничное приложение</h4>
-          <Link to={'/sign-up'} className={'portfolio__link'}>{'↗'}</Link>
+          <a href="https://dariakamenskaya.github.io/mesto/index.html" rel="noopener noreferrer" target="_blank" className="portfolio__link">{'↗'}</a>
         </li>
       </div>
     </section>
