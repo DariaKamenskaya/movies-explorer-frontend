1dc5bbb98c17c378644ff5741ad82f92e5fefe6e
Daria Kamenskaya <daria.kamenskaya@yandex.ru>
2022-06-22 22:00:53 +0300

    feat: поправить верстку формы поиска и страницы с фильмами

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -7,12 +7,17 @@ function SearchForm() {
 
   return (
     <main className="searchForm">
-      <div className="searchForm__nav">
-        <img src={iconSearch} alt="Поиск" className="searchForm__icon searchForm__icon-search"/>
-        <p className="searchForm__title">{'Фильм'}</p>
-        <img src={iconNext} alt="Следующий" className="searchForm__icon"/>
-      </div>
+      <form className="searchForm__nav">
+        <button type="button" className="searchForm__button-search searchForm__icon-search" ></button>
+        <input required id="name" name="name" type="text" placeholder="Фильм" className="searchForm__input-text" />
+        <button type="submit" className="searchForm__button-submit" ></button>
+      </form>
       <FilterCheckbox/>
+          {/* <div className="searchForm__nav">
+    <img src={iconSearch} alt="Поиск" className="searchForm__icon searchForm__icon-search"/>
+<p className="searchForm__title">{'Фильм'}</p>
+<img src={iconNext} alt="Следующий" className="searchForm__icon"/>
+</div> */}
     </main>
   );
 }
