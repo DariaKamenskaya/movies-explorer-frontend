f5fe544d407cbafd91f82de5ad7637811b7e345c
Daria Kamenskaya <daria.kamenskaya@yandex.ru>
2022-06-30 12:22:07 +0300

    feat: добавить запросы к апи и валидацию формы Регистрация

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,29 +1,136 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import React, { useState, useCallback, useEffect } from 'react';
 import logoUser from '../../images/logo_user.svg'; 
+import * as auth from '../../utils/auth';
 
 function Register() {
+
+  const [values, setValues] = useState({
+    name: '',
+    email: '',
+    password: ''
+  });
+  const [errors, setErrors] = React.useState({});
+  const [isValid, setIsValid] = React.useState(false);
+
+  const navigate = useNavigate();
+
+  // Обработчик изменения инпута обновляет стейт
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    const target = e.target;
+    setValues((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+    setErrors({...errors, [name]: target.validationMessage });
+    if (values['name'] === "") {
+      setErrors({...errors, [name]: target.validationMessage, 'name': 'Это поле необходимо заполнить' });
+    }
+    if (values['email'] === "") {
+      setErrors({...errors, [name]: target.validationMessage, 'email': 'Это поле необходимо заполнить' });
+    }
+    if (values['password'] === "") {
+      setErrors({...errors, [name]: target.validationMessage, 'password': 'Это поле необходимо заполнить' });
+    }
+    setIsValid(target.closest("form").checkValidity());
+  };
+
+  const resetForm = useCallback(
+    (newValues = {}, newErrors = {}, newIsValid = false) => {
+      setValues(newValues);
+      setErrors(newErrors);
+      setIsValid(newIsValid);
+    },
+    [setValues, setErrors, setIsValid]
+  );
+
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(values.name,values.email,values.password)
+    auth
+        .createUser(
+          values.name,
+          values.email,
+          values.password
+        )
+        .then((res) => {
+          if(res.data){
+            console.log('Регистрация прошла успешно');
+            navigate('/movies');
+          } else {
+            console.log('Ой, что-то все упало');
+          }
+        })
+        .catch((err) => {
+          console.log(err); // "Что-то пошло не так: ..."
+          return [];
+        });
+  };
+
+
+
+  useEffect(() => {
+    const initialValue = {
+      name: '',
+      email: '',
+      password: ''
+    }
+    const initialErrors = {
+      name: '',
+      email: '',
+      password: ''
+    }
+    resetForm(initialValue,initialErrors,false);
+  }, [])
+
 
   return (
     <div>
       <Link to={'/'}><img src={logoUser} alt="Логотип пользователя" className="welcome__logo"/></Link>
       <h1 className="welcome-title welcome-title_login">Добро пожаловать!</h1>
-      <form className="login__form">
+      <form className="login__form" onSubmit={handleSubmit}>
         <div className="login__form-input">
           <label className="login__label-text">{"Имя"}</label>
-          <input required id="name" name="name" type="text" placeholder="Имя" className="login__input-text"
-            value={"Виталий"} />
+          <input  required="required"
+                  minLength="2"
+                  maxLength="40" 
+                  id="name"
+                  name="name"
+                  type="text"
+                  placeholder="Имя"
+                  className="login__input-text"
+                  value={values.name}
+                  onChange={handleChange}/>
+          <span id="form-input-error" className="form__input-error">{errors.name}</span>
         </div>
         <div className="login__form-input">
           <label className="login__label-text">{"Email"}</label>
-          <input required id="email" name="email" type="email" placeholder="Email" className="login__input-text"
-            value={"pochta@yandex.ru"} />
+          <input required
+                 id="email"
+                 name="email"
+                 type="email"
+                 placeholder="Email"
+                 className="login__input-text"
+                 value={values.email}
+                 onChange={handleChange}/>
+          <span id="form-input-error" className="form__input-error">{errors.email}</span>
         </div>
         <div className="login__form-input">
           <label className="login__label-text">{"Пароль"}</label>
-          <input required id="password" name="password" type="password" placeholder="Пароль" className="login__input-text"
-            value={"Виталий"} />
+          <input required
+                 id="password"
+                 name="password"
+                 type="password"
+                 placeholder="Пароль"
+                 className="login__input-text"
+                 value={values.password}
+                 onChange={handleChange}/>
+          <span id="form-input-error" className="form__input-error">{errors.password}</span>
         </div>
-        <button type="submit" className="login__button" >Зарегистрироваться</button>
+        <button type="submit" className= {isValid ?  "login__button-active" : "login__button-inactive"} disabled={!isValid} >Зарегистрироваться</button>
       </form>
       <div className="login__link-container">
         <p className="welcome-link welcome-link_grey">Уже зарегистрированы?</p>
---
+++ src/utils/auth.js
@@ -0,0 +1,50 @@
+import {baseUrlApiMain} from "../utils/constant";
+
+
+export const createUser = (name, email, password) => {
+  return fetch(`${baseUrlApiMain}/signup`, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({name, email, password})
+  })
+  .then((response) => {
+    return response.json();
+  })
+  .then((res) => {
+    return res;
+  })
+  .catch((err) => console.log(err));
+};
+
+
+export const authorize = (email, password) => {
+    return fetch(`${baseUrlApiMain}/signin`, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({email, password})
+    })
+    .then(res => res.json())
+    .then(data => data)
+    .catch(err => console.log(err))
+  };
+
+
+  export const checkToken = (token) => {
+    return fetch(`${baseUrlApiMain}/users/me`, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`,
+      }
+    })
+    .then(res => res.json())
+    .then(data => data)
+  }
+  
