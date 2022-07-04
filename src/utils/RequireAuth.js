7c4c744c8044efe1402fd4cfaa944c4de58c5246
Daria Kamenskaya <daria.kamenskaya@yandex.ru>
2022-07-04 21:00:39 +0300

    fix: поправить маршрут переадрисации для защищенных компонентов

--- src/utils/RequireAuth.js
+++ src/utils/RequireAuth.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Navigate } from "react-router-dom";
 
 const RequireAuth = ({ loggedIn, children  }) => {
-  return loggedIn ? children : <Navigate to="/signin" />
+  return loggedIn ? children : <Navigate to="/" />
   }
 
 export default RequireAuth;
